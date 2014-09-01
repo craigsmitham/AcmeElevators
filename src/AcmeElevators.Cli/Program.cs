0060f78b1d406e990d86dac22dc3ecbd63c9574a
Craig Smitham <craigsmitham@gmail.com>
2014-09-01 18:50:45 -0500

    initial reorg

---
+++ src/AcmeElevators.Cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeElevators.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
