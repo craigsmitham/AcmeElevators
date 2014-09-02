91b55f3f5ad0c8692c78b2d15e05c1f9aed9d5eb
Craig Smitham <craigsmitham@gmail.com>
2014-09-01 19:15:53 -0500

    Intitial refactoring

---
+++ README.md
@@ -0,0 +1,18 @@
+# Development Log
+
+## Intial Design Observations
+
+* The Elevator System interface conflates some concerns. 
+* The status query and pickup commands seem like a natural public API, but the step and update should be internal
+* We can clean up the interface and method calls to make them more intention reavealing
+* The status query does not give the ability to specify an elevator ID, or it assumes we'll only be querying one elevator. This is confusing. 
+
+## First Steps (planned)
+
+* Clean up interface to make it more idiomatic C#
+* Split control system interface into public and internal API methods
+* Add an elevator ID status to the status query
+
+## First Steps (actual)
+
+*
---
+++ src/AcmeElevators/IElevatorControlSystem.cs
@@ -0,0 +1,51 @@
+﻿namespace AcmeElevators
+{
+    public interface IElevatorControlSystem : IPublicElevatorControls, IInternalElevatorControls { }
+
+    public interface IPublicElevatorControls
+    {
+        /// <summary>
+        /// Get an elevator's status 
+        /// </summary>
+        /// <param name="elevatorId">The ID for the desired elevator</param>
+        /// <returns>The status for the specified elevator</returns>
+        IElevatorStatus GetStatus(int elevatorId);
+
+        /// <summary>
+        /// Request an elevator pickup
+        /// </summary>
+        /// <param name="currentFloorNumber">The floor number</param>
+        /// <param name="direction">The desired elevator direction</param>
+        void Pickup(int currentFloorNumber, Direction direction);
+    }
+
+    public interface IInternalElevatorControls
+    {
+        /// <summary>
+        /// Update the elevator state
+        /// </summary>
+        /// <param name="elevatorId">Desired elevator ID</param>
+        /// <param name="floorNumber">Change the floor number</param>
+        /// <param name="goalFloorNumber">Change the goal floor number</param>
+        void Update(int elevatorId, int floorNumber, int goalFloorNumber);
+
+        /// <summary>
+        /// Time step forward
+        /// </summary>
+        void Step();
+    }
+
+    public interface IElevatorStatus
+    {
+        int ElevatorId { get; }
+        int FloorNumber { get; }
+        int GoalFloorNumber { get; }
+        Direction Direction { get; }
+    }
+
+    public enum Direction
+    {
+        Up = 1,
+        Down = -1
+    }
+}
