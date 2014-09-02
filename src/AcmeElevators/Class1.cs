4d161488cbfcf84fe3f44216d70bcb75646b3f7a
Craig Smitham <craigsmitham@gmail.com>
2014-09-01 19:06:33 -0500

    Adding notes

--- src/AcmeElevators/Class1.cs
+++ src/AcmeElevators/Class1.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace AcmeElevators
 {
-    public class Class1
+    public interface IElevatorControlSystem
     {
+        /// <summary>
+        /// Returns the elevator status
+        /// </summary>
+        /// <returns>A tuple containing the Elevator ID, Floor Number, and Goal Floor Number</returns>
+        Tuple<int, int, int> Status();
+
+        /// <summary>
+        /// Update the elevator state
+        /// </summary>
+        /// <param name="elevatorId">Desired elevator ID</param>
+        /// <param name="floorNumber">Change the floor number</param>
+        /// <param name="goalFloorNumber">Change the goal floor number</param>
+        void Update(int elevatorId, int floorNumber, int goalFloorNumber);
+        void Pickup(int floorNumber, int direction);
+        void Step();
     }
 }
